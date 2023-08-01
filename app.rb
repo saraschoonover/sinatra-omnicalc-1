c00ed2005fafc7a4b955537cb43cdcea8e875067
Sara Schoonover <Saramschoon@gmail.com>
2023-08-01 17:42:01 +0000

    created square with form

--- app.rb
+++ app.rb
@@ -2,8 +2,9 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+ erb(:layout)
+end
+
+get("/square/new") do
+  erb(:square_new)
 end
