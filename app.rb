3e21f16e4afd6d1623e83be10676322fd6e6fa00
Sara Schoonover <Saramschoon@gmail.com>
2023-08-02 17:18:16 +0000

    completed payment & reformatted code

--- app.rb
+++ app.rb
@@ -1,8 +1,9 @@
 require "sinatra"
 require "sinatra/reloader"
+#require "./config/environment"
 
 get("/") do
- erb(:layout)
+ erb(:random)
 end
 
 get("/square/new") do
@@ -34,12 +35,22 @@ get("/random/new") do
 end
 
 get("/process_url/payment") do
-  @apr = params.fetch("apr").to_f / 100
+  @apr = params.fetch("apr").to_f
   @num_of_years = params.fetch("year").to_i
+
   @principal = params.fetch("principal").to_i
-  @number_of_periods = @num_of_years * 12
-  
-  @payment = 
+
+  periods_remaining = @num_of_years * 12
+  monthly_interest_rate = @apr.round(4) / 100 / 12
+  numerator = @principal * monthly_interest_rate * (1 + monthly_interest_rate)** periods_remaining
+  denominator = (1 + monthly_interest_rate)** periods_remaining - 1
+
+  @monthly_payment = numerator / denominator
+
+  @formatted_apr = sprintf("%.4f", @apr)
+  # @formatted_principal = sprintf("$%.2f", @principal)
+  #@formatted_principalll = number_to_currency(@principal, precision: 2)
+ 
   erb(:submitted_payment)
 end
 
